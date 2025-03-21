785614f44e3ecbbeaf8ea71a517f6b64a62f6661
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-03-21 19:55:26 -0300

    first commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,41 @@
 class Car:
     # write your code here
-    pass
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str) -> None:
+
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
     # write your code here
-    pass
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: str, average_rating: float,
+            count_of_ratings: int) -> None:
+
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, list_cars: list) -> int:
+
+        pass
+
+    def calculate_washing_price(self) -> int:
+
+        pass
+
+    def wash_single_car(self) -> None:
+
+        pass
+
+    def rate_service(self) -> None:
+
+        pass
