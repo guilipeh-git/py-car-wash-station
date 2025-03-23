0aa3142ed2aa757b5d29e63baf2c18f26cc84955
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-03-23 20:39:17 -0300

    correcao

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ class CarWashStation:
                 total += self.calculate_washing_price(car)
                 self.wash_single_car(car)
 
-        return total
+        return round(total, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         calc = car.comfort_class * (self.clean_power - car.clean_mark)
@@ -44,7 +44,7 @@ class CarWashStation:
         car.clean_mark = self.clean_power
 
     def rate_service(self, taxa: int) -> None:
-        self.average_rating = self.average_rating * self.count_of_ratings
+        self.average_rating *= self.count_of_ratings
         self.average_rating += taxa
         self.average_rating /= self.count_of_ratings + 1
         self.average_rating = round(self.average_rating, 1)
